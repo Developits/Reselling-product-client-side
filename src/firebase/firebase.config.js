68a55300f07dbe0eb76598b28527cb3a149e1460
Developits <nahidnabil1234@gmail.com>
2022-11-27 15:42:22 +0600

    Firefase authentication connected

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAf-eEl9Vyd8WweCuvYWmCZrolcq64HNfM",
+  authDomain: "salvage-yard-1ab9c.firebaseapp.com",
+  projectId: "salvage-yard-1ab9c",
+  storageBucket: "salvage-yard-1ab9c.appspot.com",
+  messagingSenderId: "762284952602",
+  appId: "1:762284952602:web:ba67e5bbddafb637ab55a3",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
