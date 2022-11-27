39d9facbe9ba256b55e8ba3b0463504d23209f5a
Developits <nahidnabil1234@gmail.com>
2022-11-27 15:30:43 +0600

    Banner Added successfully

---
+++ src/pages/Home/Banner/Banner.js
@@ -0,0 +1,12 @@
+import React from "react";
+import banner from "../../../assets/banner/Banner.png";
+
+const Banner = () => {
+  return (
+    <div>
+      <img className="lg:px-28 lg:py-4 " src={banner} alt="" />
+    </div>
+  );
+};
+
+export default Banner;
