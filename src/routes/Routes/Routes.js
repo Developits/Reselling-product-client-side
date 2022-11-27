0bb229925a2a2f393cfb306ee68cb484e68527f9
Developits <nahidnabil1234@gmail.com>
2022-11-27 15:43:53 +0600

    Complete route setup done

---
+++ src/routes/Routes/Routes.js
@@ -0,0 +1,94 @@
+import { createBrowserRouter } from "react-router-dom";
+import DashboardLayout from "../../layout/DashboardLayout";
+import Main from "../../layout/Main";
+import Blogs from "../../pages/Blogs/Blogs";
+import AddProduct from "../../pages/Dashboard/AddProduct/AddProduct";
+import AllBuyers from "../../pages/Dashboard/AllBuyers/AllBuyers";
+import AllSellers from "../../pages/Dashboard/AllSellers/AllSellers";
+import MyBuyers from "../../pages/Dashboard/MyBuyers/MyBuyers";
+import MyOrders from "../../pages/Dashboard/MyOrders/MyOrders";
+import MyProducts from "../../pages/Dashboard/MyProducts/MyProducts";
+import ReportedItems from "../../pages/Dashboard/ReportedItems/ReportedItems";
+import Home from "../../pages/Home/Home/Home";
+import Login from "../../pages/Login/Login";
+import Products from "../../pages/Products/Products";
+import ErrorPage from "../../pages/Shared/ErrorPage/ErrorPage";
+import Signup from "../../pages/Signup/Signup";
+import PrivateRoute from "../PrivateRoute/PrivateRoute";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+      {
+        path: "/signup",
+        element: <Signup></Signup>,
+      },
+      {
+        path: "/blogs",
+        element: <Blogs></Blogs>,
+      },
+      {
+        path: "/category/:id",
+        element: (
+          <PrivateRoute>
+            <Products></Products>
+          </PrivateRoute>
+        ),
+        loader: ({ params }) =>
+          fetch(`http://localhost:5000/products/${params.id}`),
+      },
+    ],
+  },
+  {
+    path: "/dashboard",
+    element: (
+      <PrivateRoute>
+        <DashboardLayout></DashboardLayout>
+      </PrivateRoute>
+    ),
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: "/dashboard/myorders",
+        element: <MyOrders></MyOrders>,
+      },
+      {
+        path: "/dashboard/myproducts",
+        element: <MyProducts></MyProducts>,
+      },
+      {
+        path: "/dashboard/mybuyers",
+        element: <MyBuyers></MyBuyers>,
+      },
+      {
+        path: "/dashboard/addproduct",
+        element: <AddProduct></AddProduct>,
+      },
+      {
+        path: "/dashboard/allsellers",
+        element: <AllSellers></AllSellers>,
+      },
+      {
+        path: "/dashboard/allbuyers",
+        element: <AllBuyers></AllBuyers>,
+      },
+      {
+        path: "/dashboard/reporteditems",
+        element: <ReportedItems></ReportedItems>,
+      },
+    ],
+  },
+]);
+
+export default router;
